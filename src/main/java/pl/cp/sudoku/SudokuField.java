4dad2b4e6085d2d3c3e2f3da518fb04aebc87ba6
234749 Maciej Adamczewski <234749@edu.p.lodz.pl>
2022-03-28 10:43:53 +0200

    Object implementation.

---
+++ src/main/java/pl/cp/sudoku/SudokuField.java
@@ -0,0 +1,21 @@
+package pl.cp.sudoku;
+
+public class SudokuField extends Observable {
+
+    private int value;
+    private Observer observer;
+
+    public SudokuField(Observer observer) {
+        super(observer);
+        this.value = 0;
+    }
+
+    public int getFieldValue() {
+        return this.value;
+    }
+
+    public void setFieldValue(int value) {
+        this.value = value;
+        notifyObservers();
+    }
+}
